18778a4b8eaf79bbc42daca649267a0753da770a
vvoo_ds <48902356+JungWooChul@users.noreply.github.com>
2024-01-17 23:18:22 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.py
@@ -0,0 +1,24 @@
+def solution(s):
+    return ' '.join([case.capitalize() for case in s.lower().split(' ')])
+#     answer = ''
+    
+#     # 입력 문자열 전체를 소문자로 변환
+#     s = s.lower()
+    
+#     # 공백 확인
+#     check = 1
+    
+#     # 전체 탐색
+#     for alp in s:
+#         # 공백인 경우는 문자열에 그대로 포함시키고, check
+#         if alp == ' ':
+#             answer += alp
+#             check = 1
+#         # check가 1인 경우(공백 다음 오는 단어)에는 문자를 대문자로 변경하여 포함
+#         else:
+#             if check == 1:
+#                 answer += alp.upper()
+#                 check = 0
+#             else:
+#                 answer += alp
+#     return answer
