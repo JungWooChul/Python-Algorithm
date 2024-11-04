c2d100b2a3400bb7841fe582d2b91a4eabdde4b5
vvoo_ds <48902356+JungWooChul@users.noreply.github.com>
2024-11-05 00:33:39 +0900

    [Gold V] Title: 평범한 배낭, Time: 1888 ms, Memory: 34972 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
@@ -2,21 +2,16 @@ import sys
 input  = sys.stdin.readline
 
 def common_bag(N, K, arr):
-    dp = [[0]*(K+1) for _ in range(N+1)]
+    dp = [0] * (K + 1)
+    for weight, value in arr:
+        for j in range(K, weight - 1, -1):
+            dp[j] = max(dp[j], dp[j - weight] + value)
 
-    for i in range(1, N+1):
-        for j in range(1,K+1):
-            if arr[i-1][0] > j:
-                dp[i][j] = dp[i-1][j]
-            else:
-                dp[i][j] = max(dp[i-1][j], dp[i-1][j-arr[i-1][0]] + arr[i-1][1])
-
-    return dp[N][K]
+    return dp[K]
 
 def main():
     N, K = map(int, input().strip().split())
     arr = [list(map(int, input().strip().split())) for _ in range(N)]
-    arr.sort(key=lambda x:x[0])
     
     print(common_bag(N, K, arr))
 
