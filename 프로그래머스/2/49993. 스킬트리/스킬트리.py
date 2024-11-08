080da3783a0a4d607cf2450beae7c018c2b4e2c5
vvoo_ds <48902356+JungWooChul@users.noreply.github.com>
2024-11-08 18:24:17 +0900

    [level 2] Title: 스킬트리, Time: 0.03 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/49993. 스킬트리/스킬트리.py
+++ 프로그래머스/2/49993. 스킬트리/스킬트리.py
@@ -1,3 +1,19 @@
 def solution(skill, skill_trees):
-    answer = -1
+    answer = 0
+    for skill_tree in skill_trees:
+        idx, pnp = 0, 0 # check: 사용할 수 있는 스킬의 종류(인덱스), pnp: pass or fail 
+        for branch in skill_tree:
+            if skill[idx] == branch:
+                idx += 1
+                if idx == len(skill):
+                    break
+            else:
+                if branch in skill:
+                    pnp += 1
+                    break
+                else:
+                    continue
+        if not pnp:
+            answer += 1
+    
     return answer
