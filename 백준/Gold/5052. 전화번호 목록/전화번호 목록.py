5cc0fbd32d21f282fd1717cb5f3fe7054596fb70
vvoo_ds <48902356+JungWooChul@users.noreply.github.com>
2024-11-21 20:19:54 +0900

    [Gold IV] Title: 전화번호 목록, Time: 124 ms, Memory: 32140 KB -BaekjoonHub

--- 백준/Gold/5052. 전화번호 목록/전화번호 목록.py
+++ 백준/Gold/5052. 전화번호 목록/전화번호 목록.py
@@ -1,20 +1,25 @@
-def is_consistent(phone_numbers):
-    phone_numbers.sort()  # 사전순 정렬
+import sys
+input = sys.stdin.readline
+
+def is_prefix(phone_numbers):
+    phone_numbers.sort() # 사전순 정렬
     for i in range(len(phone_numbers) - 1):
         # 현재 번호가 다음 번호의 접두어인지 확인
         if phone_numbers[i + 1].startswith(phone_numbers[i]):
             return "NO"
     return "YES"
 
-# 테스트 케이스 입력 및 처리
-T = int(input())  # 테스트 케이스 수
-results = []
+def main():
+    T = int(input())
+    results = []
 
-for _ in range(T):
-    n = int(input())  # 전화번호 개수
-    phone_numbers = [input().strip() for _ in range(n)]
-    results.append(is_consistent(phone_numbers))
+    for _ in range(T):
+        n = int(input())
+        phone_numbers = [input().strip() for _ in range(n)]
+        results.append(is_prefix(phone_numbers))
 
-# 결과 출력
-for result in results:
-    print(f"{result}")
+    for result in results:
+        print(f"{result}")
+
+if __name__ == '__main__':
+    main()
