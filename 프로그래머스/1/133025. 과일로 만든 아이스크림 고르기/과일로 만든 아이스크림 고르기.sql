355e21f3a5b29f90a3576f5d22b42e8bdac39280
vvoo_ds <48902356+JungWooChul@users.noreply.github.com>
2025-01-12 17:46:27 +0900

    Update 과일로 만든 아이스크림 고르기.sql

--- 프로그래머스/1/133025. 과일로 만든 아이스크림 고르기/과일로 만든 아이스크림 고르기.sql
+++ 프로그래머스/1/133025. 과일로 만든 아이스크림 고르기/과일로 만든 아이스크림 고르기.sql
@@ -1,8 +1,8 @@
 -- 코드를 입력하세요 
 SELECT FLAVOR
-FROM (SELECT A.SHIPMENT_ID, A.FLAVOR, A.TOTAL_ORDER, B.INGREDIENT_TYPE
-      FROM (FIRST_HALF as A
+FROM (SELECT A.SHIPMENT_ID, A.FLAVOR, A.TOTAL_ORDER, B.INGREDIENT_TYPE # join 진행 시 컬럼명 재정의
+      FROM (FIRST_HALF as A # AS로 테이블명 재정의
             JOIN ICECREAM_INFO as B
       ON a.FLAVOR = b.FLAVOR)
-      WHERE TOTAL_ORDER > 3000 AND INGREDIENT_TYPE = 'fruit_based') favorite_fruit_based
+      WHERE TOTAL_ORDER > 3000 AND INGREDIENT_TYPE = 'fruit_based') favorite_fruit_based # 서브 쿼리 작성시 새롭게 생성된 테이블명 선언
 ORDER BY TOTAL_ORDER DESC;
