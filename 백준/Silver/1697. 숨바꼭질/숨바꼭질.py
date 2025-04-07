bce4cdf1d9747ebef1e18ebcb38d67883bd110f3
vvoo_ds <48902356+JungWooChul@users.noreply.github.com>
2025-04-08 04:33:12 +0900

    [Silver I] Title: 숨바꼭질, Time: 92 ms, Memory: 38164 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
@@ -3,55 +3,22 @@ from collections import deque, defaultdict
 input = sys.stdin.readline
 
 def main():
-    n,k = map(int, input().strip().split())
+    n, k = map(int, input().strip().split())
+    max_num = 100000
+    discovered = [0]*(max_num+1)
 
-    if n < k:
-        graph = [float('inf')] * (2*k+1)
-        discovered = [0]*(2*k+1)
+    def bfs(n,k):
         queue = deque()
-
-        if n == 0:
-            graph[n] = 0
-            discovered[n] = 1
-            queue.append(n)
-        else:
-            mul, cnt = n, 0
-            while mul < k:
-                graph[mul] = cnt
-                discovered[mul] = 1
-                queue.append(mul)
-                mul *= 2
-                cnt += 1
+        queue.append(n)
 
         while queue:
             x = queue.popleft()
+            if x == k:
+                print(discovered[x])
+                return
             for nx in [x-1, x+1, 2*x]:
-                if 0 <= nx <= 2*k and not discovered[nx]:
+                if 0<=nx<=max_num and not discovered[nx]:
+                    discovered[nx] = discovered[x]+1
                     queue.append(nx)
-                    discovered[nx] = 1
-
-                    graph[nx] = graph[x]+1
-    elif n > k:
-        graph = [float('inf')] * (2*n + 1)
-        discovered = [0] * (2*n + 1)
-
-        queue = deque([n])
-        discovered[n] = 1
-        graph[n] = 0
-
-        while queue:
-            x = queue.popleft()
-            for nx in [x - 1, x + 1, 2 * x]:
-                if 0<= nx<=2*n:
-                    if not discovered[nx]:
-                        queue.append(nx)
-                        discovered[nx] = 1
-
-                        graph[nx] = graph[x] + 1
-
-    else:
-        print(0)
-        return
-    print(graph[k])
-
+    bfs(n,k)
 main()
